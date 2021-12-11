9150a6613272e8008bcd9cc4e791c1cb47f5ecd5
Ingeinlens <ingeinlens@gmail.com>
2021-12-11 16:31:30 +0800

    Реализация первой части

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,65 @@ namespace Coursework
 {
     public partial class Form1 : Form
     {
+        private Emitter emitter = new Emitter(); // добавили эмиттер
+
         public Form1()
         {
             InitializeComponent();
+
+            //Добавляем привязку изображения
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // а тут теперь вручную создаем
+            emitter = new TopEmitter
+            {
+                Width = picDisplay.Width,
+                GravitationY = 0.25f
+            };
+
+            // гравитон
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.25),
+                Y = picDisplay.Height / 2
+            });
+
+            // в центре антигравитон
+            emitter.impactPoints.Add(new AntiGravityPoint
+            {
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2
+            });
+
+            // снова гравитон
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.75),
+                Y = picDisplay.Height / 2
+            });
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // обновляем состояние системы
+            emitter.UpdateState();
+
+            //Используя класс графики будет отрисовывать частицы
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black); // очищаем поле
+                emitter.Render(g);
+            }
+
+            //Обновляем картинку
+            picDisplay.Invalidate();
+        }
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            // в обработчике заносим положение мыши в переменные для хранения положения мыши
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
